8cd07f3ca17dad8c20e803f9673ffff918fcb289
dokxo96 <dokxo96@gmail.com>
2020-06-30 21:10:19 -0600

    Setting the new style to the main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from './components/login'
 import SignUp from './components/SignUp'
-
+import logo from './assets/img/Sep-bgremove.png'
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
 
@@ -17,46 +17,42 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">SEP</a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i className="fa fa-bars"></i>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive">
-        <ul className="navbar-nav text-uppercase ml-auto">
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#services">Ventajas</a>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link js-scroll-trigger" to={"/sign-in"} href="javascript:location.reload()" >Ingresar</Link>   
-          </li>
-          <li className="nav-item">
-          <Link className=" nav-link js-scroll-trigger" to={"/sign-up"}href="javascript:location.reload()"  >Registrarse</Link>       
-         </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+      <nav className="navbar navbar-expand-lg  fixed-top" id="mainNav">
+          <div className="container">
+              <div id="img-contenedor">
+                  <img src={logo} alt="" />
+              </div>
+            
+              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  Menu
+                  <i className="fa fa-bars"></i>
+              </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav text-uppercase ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link js-scroll-trigger" href="#services">Ventajas</a>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link js-scroll-trigger" to={"/sign-in"} href="javascript:location.reload()" >Ingresar</Link>   
+                </li>
+                <li className="nav-item">
+                <Link className=" nav-link js-scroll-trigger" to={"/sign-up"}href="javascript:location.reload()"  >Registrarse</Link>       
+              </li>
+              </ul>
+            </div>
+          </div>
+       </nav>
 
   
   <header className="masthead" >
     <div className="container">
       <div className="intro-text" >
-      <Router>
+       <Router>
+                      
     <div className="row">
         <div className="col-6">
         <div className="auth-wrapper">
-                <div className="auth-inner">
-                 <h3>
-                   ESTAMPADO DE CERTIFICADOS EN BLOCKCHAIN
-                 </h3>
-                 <p>Hoy en dia todos los egresados tienen la
-                   posibilidad de encontrar trabajo fuera de su nacion por lo que
-                   el estampado de certificados en Blockchain es una herramienta para
-                   satifacer esta nececidad globlal.
-                 </p>
+                <div className="intro-heading ">  ESTAMPADO DE CERTIFICADOS EN BLOCKCHAIN
                 </div>
             
             </div>   
@@ -66,7 +62,7 @@ function App() {
          <div className="auth-wrapper">
                 <div className="auth-inner">
                   <Switch>
-                    <Route exact path='/' component={Login} id="login"/>
+                    <Route exact path='/' component={SignUp}/>
                     <Route path="/sign-in" component={Login}  />
                     <Route path="/sign-up" component={SignUp} />
                   </Switch>
@@ -81,7 +77,19 @@ function App() {
     </div>
   </header>
 
-  
+  <section class="page-section bg-success" id="blockchain">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h1 class="text-white mt-0">¿Que es Blockchain?</h1>
+                        <hr class="divider light my-4" />
+                        <p class="text-white-50 mb-4">es un sistema de transferencia digital basado en la distribución de la información (o dinero) en multitud de nodos independientes (ordenadores de usuarios) que registran y validan dicha información (token) de forma anónima, eliminando intermediarios e impidiendo que la información pueda ser borrada. La base de esta tecnología es el consenso, pues si todos los nodos tienen y comparten la misma información, la toman como válida. Simple, rápido y sin intermediarios. </p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+ 
   <section className="page-section" id="services">
     <div className="container">
       <div className="row">
